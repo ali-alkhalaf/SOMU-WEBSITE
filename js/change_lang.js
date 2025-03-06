8cf172ef80335dcce63ab4f272ec4ce9df9da237
Ali AlKhalaf <139816161+ali-alkhalaf@users.noreply.github.com>
2025-03-06 10:48:14 +0300

    Add files via upload

---
+++ js/change_lang.js
@@ -0,0 +1,8 @@
+var totalsale = document.getElementById("totalsale");
+var totalpayment = document.getElementById("totalpayment");
+function totale() {
+    totalpayment == totalsale;
+    return totalpayment
+}
+
+totalpayment.innerHTML = totalsale;
